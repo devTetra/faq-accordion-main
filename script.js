09865d131c7505cec8d36e10f62653d029113c2e
Treasure Ejike <tre.ejike@gmail.com>
2024-02-27 15:47:36 +0100

    Added files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+const btns = document.querySelectorAll('.btn');
+const questions = document.querySelectorAll('.question');
+
+const absoluteSrc = src => {
+	const read = src.includes('http')
+		? src.replace('http://127.0.0.1:5500', '.')
+		: src;
+	return read;
+};
+
+const accToggle = el => {
+	const answer = el.parentElement.nextElementSibling;
+	const src = absoluteSrc(el.src);
+	if (src === './images/icon-plus.svg') {
+		el.src = './images/icon-minus.svg';
+		answer.style.display = 'block';
+	} else {
+		el.src = './images/icon-plus.svg';
+		answer.style.display = 'none';
+	}
+};
+
+btns.forEach(btn => btn.addEventListener('click', () => accToggle(btn)));
+questions.forEach(question =>
+	question.addEventListener('click', () =>
+		accToggle(question.nextElementSibling)
+	)
+);
+
+const body = document.querySelector('body');
+body.addEventListener('keydown', el => {
+	console.log(el);
+});
