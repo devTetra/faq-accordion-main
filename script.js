bf5dcaace1b633ecef1fcc78e253d9ea4eb3d7f0
Treasure Ejike <tre.ejike@gmail.com>
2024-02-28 08:34:53 +0100

    Test 1

--- script.js
+++ script.js
@@ -2,29 +2,42 @@
 const btns = document.querySelectorAll('.btn');
 const questions = document.querySelectorAll('.question');
 
+// const absoluteSrc = src => {
+// 	const read = src.includes('icon-plus.svg')
+// 		? src.replace('icon-plus.svg', 'icon-minus.svg')
+// 		: src.replace('icon-minus.svg', 'icon-plus.svg');
+// 	return read;
+// };
+
+// const accToggle = el => {
+// 	const parent = el.parentElement.parentElement;
+// 	parent.classList.toggle('close');
+// 	const src = absoluteSrc(el.src);
+// 	src === './images/icon-plus.svg'
+// 		? (el.src = './images/icon-minus.svg')
+// 		: (el.src = './images/icon-plus.svg');
+// 	console.log(src);
+// };
+
 const absoluteSrc = src => {
-	const read = src.includes('https')
-		? src.replace('https://devtetra.github.io/faq-accordion-main', '.')
-		: src;
-	return read;
+	src = src.includes('icon-plus.svg')
+		? src.replace('icon-plus.svg', 'icon-minus.svg')
+		: src.replace('icon-minus.svg', 'icon-plus.svg');
+	return src;
 };
 
 const accToggle = el => {
 	const parent = el.parentElement.parentElement;
 	parent.classList.toggle('close');
-	const src = absoluteSrc(el.src);
-	src === './images/icon-plus.svg'
-		? (el.src = './images/icon-minus.svg')
-		: (el.src = './images/icon-plus.svg');
-	console.log(src);
+	el.src = absoluteSrc(el.src);
 };
 
 btns.forEach(btn => btn.addEventListener('click', () => accToggle(btn)));
-questions.forEach(question =>
-	question.addEventListener('click', () =>
-		accToggle(question.nextElementSibling)
-	)
-);
+// questions.forEach(question =>
+// 	question.addEventListener('click', () =>
+// 		accToggle(question.nextElementSibling)
+// 	)
+// );
 
 // btns.forEach(btn => btn.addEventListener('touchstart', () => accToggle(btn)));
 // questions.forEach(question =>
