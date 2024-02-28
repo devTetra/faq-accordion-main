a1e7efd8d40875f651620314a66c75d84721443b
Treasure Ejike <tre.ejike@gmail.com>
2024-02-28 08:39:17 +0100

    Test 2

--- script.js
+++ script.js
@@ -2,23 +2,6 @@
 const btns = document.querySelectorAll('.btn');
 const questions = document.querySelectorAll('.question');
 
-// const absoluteSrc = src => {
-// 	const read = src.includes('icon-plus.svg')
-// 		? src.replace('icon-plus.svg', 'icon-minus.svg')
-// 		: src.replace('icon-minus.svg', 'icon-plus.svg');
-// 	return read;
-// };
-
-// const accToggle = el => {
-// 	const parent = el.parentElement.parentElement;
-// 	parent.classList.toggle('close');
-// 	const src = absoluteSrc(el.src);
-// 	src === './images/icon-plus.svg'
-// 		? (el.src = './images/icon-minus.svg')
-// 		: (el.src = './images/icon-plus.svg');
-// 	console.log(src);
-// };
-
 const absoluteSrc = src => {
 	src = src.includes('icon-plus.svg')
 		? src.replace('icon-plus.svg', 'icon-minus.svg')
@@ -33,11 +16,11 @@ const accToggle = el => {
 };
 
 btns.forEach(btn => btn.addEventListener('click', () => accToggle(btn)));
-// questions.forEach(question =>
-// 	question.addEventListener('click', () =>
-// 		accToggle(question.nextElementSibling)
-// 	)
-// );
+questions.forEach(question =>
+	question.addEventListener('click', () =>
+		accToggle(question.nextElementSibling)
+	)
+);
 
 // btns.forEach(btn => btn.addEventListener('touchstart', () => accToggle(btn)));
 // questions.forEach(question =>
